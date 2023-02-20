a0c12f4e11dde379441d89ae81370f34fbb22727
William Firmino <williamwf07@gmail.com>
2023-02-20 03:52:29 -0300

    feat: created layout using css and adding svg icon

--- src/Components/DetailsCard/index.jsx
+++ src/Components/DetailsCard/index.jsx
@@ -1,38 +1,58 @@
 import * as Styles from "./styles";
+import flagImgExample from "../../assets/flag.jpg";
 
 export function DetailsCard() {
   return (
     <Styles.DetailsCard>
-      <Styles.BackButton>Back</Styles.BackButton>
-      <Styles.FlagImg />
+      <Styles.BackButton>
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="ionicon"
+            viewBox="0 0 512 512"
+          >
+            <title>Arrow Back</title>
+            <path
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="48"
+              d="M244 400L100 256l144-144M120 256h292"
+            />
+          </svg>
+        </span>
+        Back
+      </Styles.BackButton>
+      <Styles.FlagImg src={flagImgExample} alt={"Flag Img Example"} />
       <Styles.CountryInfoCard>
+        <Styles.CountryName>Belgium</Styles.CountryName>
         <div>
-          <Styles.CountryName>Belgium</Styles.CountryName>
           <Styles.Info>
-            <span>Native Name:</span>België
+            <span>Native Name: </span>België
           </Styles.Info>
           <Styles.Info>
-            <span>Population:</span>11,319,511
+            <span>Population: </span>11,319,511
           </Styles.Info>
           <Styles.Info>
-            <span>Region:</span>Europe
+            <span>Region: </span>Europe
           </Styles.Info>
           <Styles.Info>
-            <span>Sub Region:</span>Western Europe
+            <span>Sub Region: </span>Western Europe
           </Styles.Info>
           <Styles.Info>
-            <span>Capital:</span>Brussels
+            <span>Capital: </span>Brussels
           </Styles.Info>
         </div>
         <div>
           <Styles.Info>
-            <span>Top Level Domain:</span>.be
+            <span>Top Level Domain: </span>.be
           </Styles.Info>
           <Styles.Info>
-            <span>Currencies:</span>Euro
+            <span>Currencies: </span>Euro
           </Styles.Info>
           <Styles.Info>
-            <span>Languages:</span>Dutch, French, German
+            <span>Languages: </span>Dutch, French, German
           </Styles.Info>
         </div>
         <div>
---
+++ src/Components/DetailsCard/styles.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import * as colors from "../../styles/variables";
+
+export const DetailsCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 6vh;
+`;
+export const BackButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-left: 7.5%;
+  margin-top: 6vh;
+
+  width: 28vw;
+  height: 4vh;
+
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 1.3rem;
+
+  align-self: flex-start;
+
+  box-shadow: 0px 0px 5px 0px rgb(0 0 0 / 40%);
+  border: none;
+
+  background-color: ${colors.white};
+
+  span svg {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 1.3rem;
+    margin-right: 0.8rem;
+  }
+`;
+export const FlagImg = styled.img`
+  width: 85%;
+`;
+export const CountryInfoCard = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 3vh;
+
+  width: 85%;
+`;
+export const CountryName = styled.h1`
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 2.2rem;
+`;
+export const Info = styled.p`
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 1.3rem;
+  color: ${colors.veryDarkBlueText};
+
+  margin-bottom: 0.5rem;
+
+  span {
+    font-weight: bold;
+  }
+`;
+export const BorderCountriesTitle = styled.p`
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 1.4rem;
+  font-weight: bold;
+`;
+export const BorderCountriesCard = styled.div``;
+export const BorderCountriesButton = styled.button``;
